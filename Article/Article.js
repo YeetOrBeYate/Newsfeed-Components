dbaa4c0fd21c3f054ab8110c2ed38c8e7efc8d91
Kyle Corbin <kylecorbin26@gmail.com>
2019-10-02 16:12:47 -0400

    Finished the articles portion of assignment

--- Article/Article.js
+++ Article/Article.js
@@ -37,9 +37,9 @@ const data = [
         mewing kittens Remus Lupin. Palominos scarlet train black robes, Metamorphimagus Niffler dead easy second bedroom. Padma
         and Parvati Sorting Hat Minister of Magic blue turban remember my last.`,
 
-    thirdParagraph: `Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights 
-        Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven 
-        roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot 
+    thirdParagraph: `Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights
+        Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven
+        roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
@@ -66,8 +66,8 @@ const data = [
         consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidorino Lorem ipsum dolor
         sit amet, consectetur adipiscing elit. Nidoking Lorem ipsum`,
 
-    thirdParagraph: `Gotta catch 'em all Horsea gym Ninjask Absol Sinnoh Poliwag. Gotta catch 'em all Youngster wants to fight Soda Pop Floatzel 
-        Leech Life Seismitoad Ariados. Earthquake Pokemon Glitch City Tail Whip Skitty Ekans Dialga. Ut aliquip ex ea commodo consequat James 
+    thirdParagraph: `Gotta catch 'em all Horsea gym Ninjask Absol Sinnoh Poliwag. Gotta catch 'em all Youngster wants to fight Soda Pop Floatzel
+        Leech Life Seismitoad Ariados. Earthquake Pokemon Glitch City Tail Whip Skitty Ekans Dialga. Ut aliquip ex ea commodo consequat James
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
@@ -88,8 +88,8 @@ const data = [
   }
 ];
 
-/* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
-  
+/* Step 1: Create a function that creates a component. You will want your component to look like the template below:
+
   <div class="article">
     <h2>{title of the article}</h2>
     <p class="date">{date of the article}</p>
@@ -102,6 +102,50 @@ const data = [
   Hint: You will need to use createElement more than once here!
 
   Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
+  */
+  function makeComponent(title,date,first,second,third){
+    const article = document.createElement('div');
+      const aTitle = document.createElement('h2');
+      const dP = document.createElement('p');
+      const fP = document.createElement('p');
+      const sP = document.createElement('p');
+      const tP = document.createElement('p');
+      const expand = document.createElement('span');
+    //give classes
+    article.classList.add('article');
+    dP.classList.add('date');
+    expand.classList.add('expandButton');
+    //give values
+    aTitle.innerText = title;
+    dP.innerText = date;
+    fP.innerText = first;
+    sP.innerText = second;
+    tP.innerText = third;
+    expand.innerText= "See more..."
+    //append in order
+    article.appendChild(aTitle);
+    article.appendChild(dP);
+    article.appendChild(fP);
+    article.appendChild(sP);
+    article.appendChild(tP);
+    article.appendChild(expand);
+
+    expand.addEventListener('click', (e)=>{
+      article.classList.toggle('article-open');
+    })
+
+    return article;
+
+  }
+
+  const accordion = document.querySelector('.articles');
+
+  data.forEach((d)=>{
+    accordion.appendChild(makeComponent(d.title,d.date,d.firstParagraph,d.secondParagraph,d.thirdParagraph))
+  })
+
+
+  /*
 
   Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
 
